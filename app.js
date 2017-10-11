5d8d634eae7bbd74aebab95c626788c56ce61771
Braxton Reid <reid74@purdue.edu>
2017-10-11 19:38:52 -0400

    clean up javascript

--- app.js
+++ app.js
@@ -6,28 +6,37 @@ box.style.background = "black";
 const container = document.querySelector("#main");
 container.appendChild(box);
 */
-const container = document.querySelector("#main");
-const gridSize = 3;
-for (let row = 0; row < gridSize; row++){
-  createRow(row);
-  for (let column = 0; column < gridSize; column++){
-    createColumn(row, column);
+class App {
+
+  constructor(){
+    this.container = document.querySelector("#main");
+    this.gridSize = 30;
+    for (let row = 0; row < this.gridSize; row++){
+      this.createRow(row);
+      for (let column = 0; column < this.gridSize; column++){
+        this.createColumn(row, column);
+      }
+    }
   }
+
+  createRow(rowID){
+    const div = document.createElement("div");
+    div.style.height=`${this.container.clientHeight/this.gridSize}px`
+    div.classList.add("row");
+    div.dataset.row=`${rowID}`
+    this.container.appendChild(div);
+  }
+
+  createColumn(rowID,columnID){
+    const ID = this.gridSize * rowID + columnID;
+    const div = document.createElement("div");
+    div.style.width=`${this.container.clientWidth/this.gridSize}px`
+    div.classList.add("column");
+    div.dataset.column=`${ID}`;
+    div.style.background = `rgb(${Math.round(Math.random() * 255)}, ${Math.round(Math.random() * 255)}, ${Math.round(Math.random() * 255)})`
+    document.querySelector(`[data-row='${rowID}']`).appendChild(div);
+  }
+  
 }
 
-function createRow(rowID){
-  const div = document.createElement("div");
-  div.style.height=`${container.clientHeight/gridSize}px`
-  div.classList.add("row");
-  div.dataset.row=`${rowID}`
-  container.appendChild(div);
-}
-
-function createColumn(rowID,columnID){
-  const ID = gridSize * rowID + columnID;
-  const div = document.createElement("div");
-  div.style.width=`${container.clientWidth/gridSize}px`
-  div.classList.add("column");
-  div.dataset.column=`${ID}`;
-  document.querySelector(`[data-row='${rowID}']`).appendChild(div);
-}
+new App();
