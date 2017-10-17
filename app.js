03503ee671b796e5cb1b4832e1fb052d2947e5dc
Braxton Reid <reid74@purdue.edu>
2017-10-16 22:55:52 -0400

    increase efficiency of board update calculations

--- app.js
+++ app.js
@@ -2,29 +2,95 @@ class App{
 
   constructor(){
     this.container = document.querySelector("#main");
-    this.gridSize = 25;
+    this.gridSize = 75;
     this.gameBoard = new Array(this.gridSize);
     for (let row = 0; row < this.gridSize; row++){
-      this.gameBoard[row] = new Array(this.gridSize);
+      //this.gameBoard[row] = new Array(this.gridSize);
       this.createRow(row);
       for (let column = 0; column < this.gridSize; column++){
-        this.gameBoard[row][column] = 0;
+        //this.gameBoard[row][column] = 0;
         this.createColumn(row, column);
       }
     }
-    this.gameBoard[11][12] = 3;
-    this.gameBoard[11][13] = 3;
-    this.gameBoard[12][11] = 3;
-    this.gameBoard[12][12] = 3;
-    this.gameBoard[13][12] = 3;
-    setInterval(() => {this.updateBoard();},100);
+
+    /*
+    this.gameBoard[11][12] = 1;
+    this.gameBoard[11][13] = 1;
+    this.gameBoard[12][11] = 1;
+    this.gameBoard[12][12] = 1;
+    this.gameBoard[13][12] = 1;
+    */
+
+    //25: [287,288,311,312,337]
+    //75: [2737,2738,2811,2812,2887]
+    this.cells = new Set([2737,2738,2811,2812,2887]);
+    this.newCells = new Set([2737,2738,2811,2812,2887]);
+    this.deadCells = new Set();
+    this.testArea = [[-1,1],[0,1],[1,1],[-1,0],[1,0],[-1,-1],[0,-1],[1,-1]];
+    setInterval(() => {this.setUpdate();},40);
   }
 
-  updateBoard(){
+  //modify this for toroidal board and square board, make new method for infinite board
+  setUpdate(){
+
+    for (const newCell of this.newCells) {
+      const cell = document.querySelector(`[data-column='${newCell}']`);
+      cell.style.background = "white"; //problem?
+    }
+
+    for (const deadCell of this.deadCells) {
+      const cell = document.querySelector(`[data-column='${deadCell}']`);
+      cell.style.background = "black";
+    }
+
+    const activeCells = [...this.newCells,...this.deadCells];
+
+    const affectedCells = new Set();
+
+    for (const activeCell of activeCells) {
+      for (let i = 0; i < this.testArea.length; i++) {
+        const cell = activeCell + this.testArea[i][0] * this.gridSize + this.testArea[i][1];
+        affectedCells.add(activeCell + this.testArea[i][0] * this.gridSize + this.testArea[i][1]);
+      }
+    }
+
+    this.newCells.clear();
+    this.deadCells.clear();
+
+    for (const affectedCell of affectedCells) {
+      if (affectedCell < 0 || affectedCell > Math.pow(this.gridSize,2)) {
+        continue;
+      }
+      let neighbors = 0;
+      for (let i = 0; i < this.testArea.length; i++) {
+        if (this.cells.has(affectedCell + this.testArea[i][0] * this.gridSize + this.testArea[i][1])) {
+          neighbors += 1;
+        }
+      }
+      if (this.cells.has(affectedCell)) {
+        if (neighbors < 2 || neighbors > 3) {
+          this.deadCells.add(affectedCell);
+        }
+      } else if (neighbors === 3) {
+        this.newCells.add(affectedCell);
+      }
+    }
+
+    for (const deadCell of this.deadCells) {
+      this.cells.delete(deadCell);
+    }
+
+    for (const newCell of this.newCells) {
+      this.cells.add(newCell);
+    }
+
+  }
+
+  update(){
     for (let i = 0; i < this.gameBoard.length; i++){
       for (let j = 0; j < this.gameBoard[i].length; j++){
         let alive = 0;
-        if (this.gameBoard[i][j] == 1 || this.gameBoard == 2){
+        if (this.gameBoard[i][j] == 2 || this.gameBoard == 3){
           alive = 1;
         }
         let neighbors = 0;
@@ -40,7 +106,7 @@ class App{
             column = 0;
           }
           while (column < this.gameBoard[row].length && column <= j + 1){
-            if (this.gameBoard[row][column] == 1 || this.gameBoard[row][column] == 2) {
+            if (this.gameBoard[row][column] == 2 || this.gameBoard[row][column] == 3) {
               neighbors += 1;
             }
             column++;
@@ -49,10 +115,10 @@ class App{
         }
         if (alive == 1) {
           if (neighbors < 3 || neighbors > 4) {
-            this.gameBoard[i][j] = 2;
+            this.gameBoard[i][j] = 3;
           }
         } else if (neighbors == 3) {
-            this.gameBoard[i][j] = 3;
+            this.gameBoard[i][j] = 1;
         }
       }
     }
@@ -60,12 +126,12 @@ class App{
       for (let j = 0; j < this.gameBoard[i].length; j++){
         const ID = this.gridSize * i + j;
         const cell = document.querySelector(`[data-column='${ID}']`);
-        if (this.gameBoard[i][j] == 2){
+        if (this.gameBoard[i][j] == 3){
           cell.style.background = "black";
           this.gameBoard[i][j] = 0;
-        } else if (this.gameBoard[i][j] == 3){
+        } else if (this.gameBoard[i][j] == 1){
           cell.style.background = "white";
-          this.gameBoard[i][j] = 1;
+          this.gameBoard[i][j] = 2;
         }
       }
     }
@@ -82,13 +148,24 @@ class App{
   createColumn(rowID,columnID){
     const ID = this.gridSize * rowID + columnID;
     const columnDiv = document.createElement("div");
-    columnDiv.style.width=`${this.container.clientWidth/this.gridSize}px`
+    columnDiv.style.width=`${this.container.clientWidth/this.gridSize}px`;
     columnDiv.classList.add("column");
     columnDiv.dataset.column=`${ID}`;
-    columnDiv.style.background = `rgb(${Math.round(Math.random() * 255)}, ${Math.round(Math.random() * 255)}, ${Math.round(Math.random() * 255)})`
+
+    let g = Math.round(Math.random() * 255);
+    let b = Math.round(Math.random() * 255);
+    while (g + b < 255){
+      g += Math.round(Math.random() * (255 - g));
+      b += Math.round(Math.random() * (255 - b));
+    }
+    columnDiv.style.background = `rgb(${0},${g},${b})`
+
+    //colomnDiv.style.background = "black";
     document.querySelector(`[data-row='${rowID}']`).appendChild(columnDiv);
   }
 
 }
+
+
 
 new App();
